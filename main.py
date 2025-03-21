086a5931444b0de0d2c3848c93c8b6ad344b7e7d
lamarcsnhs <lamarcsnhs@gmail.com>
2025-03-21 12:47:32 -0500

    Testing

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from GmailAPI import gmail as g
 import RPi.GPIO as GPIO
 import makerElectronics as me
+import time
 
 if __name__=='__main__':
     try:
-        while True:
-            print(g.checkMail())
+        # while True:
+            # print(g.checkMail())
+
+        me.spinMotor("left",37,1)
+        time.sleep(1)
+        me.spinMotor("right",31,1)
     except KeyboardInterrupt:
         pass
     finally:
         me.pwm.stop()
         GPIO.cleanup()
+
+
+        
