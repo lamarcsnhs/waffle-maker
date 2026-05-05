2be91b113d27f1934941ef07565c371d2e2bbb4c
lamarcsnhs <lamarcsnhs@gmail.com>
2026-05-05 11:49:14 -0500

    modified:   makerElectronics.py 	modified:   makerElectronics.py 	robotarm.py

--- makerElectronics.py
+++ makerElectronics.py
@@ -1,24 +1,29 @@
-#switch RPi.GPIO to adafruit's libraries
+# switch RPi.GPIO to adafruit's libraries
 
 import RPi.GPIO as GPIO
 import time
 
-#initialize GPIO
-#Based on GPIO #, not pin #
-#Motor Pins
+# initialize GPIO
+# based on GPIO #, not pin #
+
+# motor pins
 motorIN1 = 19
 motorIN2 = 12
-motorPWM = 25 #Controls Speed
-#linear actuator pins
+motorPWM = 25 # controls speed
+
+# linear actuator pins
 linearIN1 = 17
 linearIN2 = 18
-#Heat Pins
+
+# heat pin
 heatPin = 21
-#Pump Pins
+
+# pump pins
 pumpPin = 4
 
-#GPIO setup
+# GPIO setup
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 GPIO.setup(motorIN1, GPIO.OUT)
 GPIO.setup(motorIN2, GPIO.OUT)
 GPIO.setup(motorPWM, GPIO.OUT)
@@ -27,19 +32,19 @@ GPIO.setup(linearIN2, GPIO.OUT)
 GPIO.setup(heatPin, GPIO.OUT)
 GPIO.setup(pumpPin, GPIO.OUT)
 
-#PWM setup
+# PWM setup
 pwm = GPIO.PWM(motorPWM, 100)
 pwm.start(0)
 
 def spinMotor(direction, speed, duration):
     if direction == "left":
         GPIO.output(motorIN1, GPIO.HIGH)
-        # print('spinning left')
+        # print("spinning left")
         time.sleep(duration)
         GPIO.output(motorIN2, GPIO.LOW)
     elif direction == "right":
         GPIO.output(motorIN1, GPIO.LOW)
-        # print('spinning right')
+        # print("spinning right")
         time.sleep(duration)
         GPIO.output(motorIN2, GPIO.HIGH)
     
@@ -53,22 +58,23 @@ def spinMotor(direction, speed, duration):
 def moveLinearActuator(direction, duration):
     if direction == "down":
         GPIO.output(linearIN1, GPIO.HIGH)
-        print('going down')
+        print("going down")
         time.sleep(duration)
         GPIO.output(linearIN1, GPIO.LOW)
-        print('went down for', duration, 'sec' )
+        print("went down for", duration, "sec" )
     elif direction == "up":
         GPIO.output(linearIN2, GPIO.HIGH)
-        print('going up')
+        print("going up")
         time.sleep(duration+1)
         GPIO.output(linearIN2, GPIO.LOW)
-        print('went up for', duration+1, 'sec' )
+        print("went up for", duration+1, "sec" )
     GPIO.output(linearIN1, GPIO.LOW)
     GPIO.output(linearIN2, GPIO.LOW)
 
 
 def heat(duration):
     GPIO.output(heatPin, GPIO.HIGH)
+    print("heating")
     time.sleep(duration)
     GPIO.output(heatPin, GPIO.LOW)
 
@@ -83,17 +89,18 @@ def preheat(duration):
     time.sleep(2)
 
     # heat
-    heat(60)
+    heat(duration)
     time.sleep(2)
 
     # open lid
     moveLinearActuator("up", 14.5)
     time.sleep(2)
 
-def wiggle(duration):
-    for x in range(int (duration)):
-        spinMotor("left", 37, .035)
-        spinMotor("right", 37, .035)
+def wiggle(loops, duration):
+    print("wiggling")
+    for x in range(int(loops)):
+        spinMotor("left", 37, duration)
+        spinMotor("right", 37, duration)
     
 
 def run():
@@ -101,8 +108,8 @@ def run():
     moveLinearActuator("down", 14.5)
     time.sleep(2)
 
-    # heat
-    heat(230)
+    # heat (230)
+    heat(300)
     time.sleep(6)
 
     # open lid
@@ -114,16 +121,17 @@ def run():
     time.sleep(2)
 
     # wiggle motor
-    wiggle(30)
-    time.sleep(20)
-    
-
-    # fix spinning it was not doing a 180
-    # jiffy was pretty
-    
+    wiggle(70, .02)
+    time.sleep(20)    
 
     # spin motor again
-    spinMotor("right", 37, 1.55)
+    spinMotor("right", 37, 1.35)
+
+    # fix spinning it was not doing a 180
+    # jiffy was pretty good
+
+    # wiggle before to even out  batter ????
     
-#run()
-preheat()
+run()
+# preheat(60)
+#wiggle(50, .02)
