f3a0daa561f60d9aedc8a99eb18274ae4b584102
lamarcsnhs <lamarcsnhs@gmail.com>
2026-05-07 11:37:28 -0500

    modified:   robotarm.py

--- robotarm.py
+++ robotarm.py
@@ -3,6 +3,7 @@ from time import sleep
 
 arm = xarm.Controller('USB')
 sleep(0.5)
+MOVE_TIME = 1500  # changing this doesn't work :(
 
 # servo 6 = base
 # servo 5 = shoulder
@@ -11,20 +12,16 @@ sleep(0.5)
 # servo 2 = wrist_rotate
 # servo 1 = gripper
 
-MOVE_TIME = 1500  # changing this doesn't work :(
 
 def move(positions, speed=MOVE_TIME):
     """move multiple servos at once. positions is a list of [servo_id, position]."""
     arm.setPosition(positions, speed, False)
     sleep(2)
 
-
-
 def stand_straight(speed=MOVE_TIME):
     move([[2, 500], [3, 500], [4, 500], [5, 500], [6, 500]], speed)
     print("stood straight")
     
-
 
 
 def rotate_base_left(amount=200, speed=MOVE_TIME):
@@ -51,12 +48,10 @@ def shoulder_down(amount=100, speed=MOVE_TIME):
 
 
 
-
 def elbow_up(amount=200, speed=MOVE_TIME):
     pos = arm.getPosition(4)
     move([[4, pos + amount]], speed)
     print("moved elbow up", amount)
-
 
 def elbow_down(amount=200, speed=MOVE_TIME):
     pos = arm.getPosition(4)
@@ -70,12 +65,10 @@ def wrist_up(amount=200, speed=MOVE_TIME):
     move([[3, pos + amount]], speed)
     print("moved wrist up", amount)
 
-
 def wrist_down(amount=200, speed=MOVE_TIME):
     pos = arm.getPosition(3)
     move([[3, pos - amount]], speed)
     print("moved wrist down", amount)
-
 
 
 
@@ -84,11 +77,11 @@ def rotate_wrist_left(amount=200, speed=MOVE_TIME):
     move([[2, pos + amount]], speed)
     print("rotated wrist left", amount)
 
-
 def rotate_wrist_right(amount=200, speed=MOVE_TIME):
     pos = arm.getPosition(2)
     move([[2, pos - amount]], speed)
     print("rotated wrist right", amount)
+
 
 
 def close_gripper(speed=MOVE_TIME):
@@ -106,11 +99,13 @@ def relax():
     for i in range(1, 7):
         arm.servoOff(i)
 
+
 def read_all_positions():
     """print current position of all servos"""
     for i in range(1, 7):
         sleep(0.1)
         print(f"Servo {i}: {arm.getPosition(i)}")
+
 
 def test_all_servos():
     """move all the servos back and forth for testing"""
@@ -126,6 +121,8 @@ def test_all_servos():
     rotate_wrist_right()
     close_gripper()
     open_gripper()
+
+
 
 
 # HOLD BASE DOWN or it may go flying
